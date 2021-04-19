e97e4139668b1b10918559078a1988f4bfa83ff9
Minh Nghia Phan <phanminhnghia98@gmail.com>
2021-04-20 01:01:43 +0200

    No more CSV output

--- receipt-pm4py.py
+++ receipt-pm4py.py
@@ -8,15 +8,12 @@ df = pd.read_parquet(sys.argv[1])
 columns = df.columns
 columns = [x.replace("AAA", ":") for x in columns]
 df.columns = columns
-log_name = sys.argv[1].replace(".parquet", ".csv")
-df.to_csv(log_name)
-log_csv = pd.read_csv(log_name)
 
 parameters = {log_converter.Variants.TO_EVENT_LOG.value.Parameters.CASE_ID_KEY: 'case:concept:name'}
-event_log = log_converter.apply(log_csv, parameters=parameters, variant=log_converter.Variants.TO_EVENT_LOG)
+event_log = log_converter.apply(df, parameters=parameters, variant=log_converter.Variants.TO_EVENT_LOG)
 
 start = timeit.default_timer()
-dfg = dfg_discovery.apply(log_csv)
+dfg = dfg_discovery.apply(event_log)
 end = timeit.default_timer()
 print(dfg)
 print(f"It took PM4Py {end-start} seconds to discover the DFG")
